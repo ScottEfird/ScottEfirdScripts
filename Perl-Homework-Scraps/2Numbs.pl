b9fde1173eb9a9bad2c376646a1e24704f651833
Scott Efird <efirds@hotmail.com>
2016-03-25 11:29:49 -0600

    Removed, not useful to save.

--- Perl-Homework-Scraps/2Numbs.pl
+++
@@ -1,8 +0,0 @@
-print "Please enter a number: \n";
-$number1 = <STDIN>;
-chomp($number1);
-print "Please enter another number: \n";
-$number2 = <STDIN>;
-chomp($number2);
-print $number1*$number2;
-<STDIN>;
