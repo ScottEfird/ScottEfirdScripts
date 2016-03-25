a0933b5e4a8682dfa7e73518ca400e1b0ddce633
Scott Efird <efirds@hotmail.com>
2016-03-25 11:31:26 -0600

    Removed, not useful.

--- Perl-Homework-Scraps/catLab.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-$filename = $ARGV[0];
-open(fh, $filename)
-   or die "Could not open file '$filename' $!"; 
-@lines = <fh>;
-@rev = reverse(@lines);
-foreach $revLine (@rev){
-	print $revLine;
-}
