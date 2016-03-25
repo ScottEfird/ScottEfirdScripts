96581e4923febd5d5264524126f309ab49e23ce0
Scott Efird <efirds@hotmail.com>
2016-03-25 11:38:58 -0600

    Removed, added code to "Homework_Samples.pl"

--- Perl-Homework-Scraps/loop.pl
+++
@@ -1,9 +0,0 @@
-print "Please enter a number: \n";
-$number1 = <STDIN>;
-chomp($number1);
-print "Please enter a string: \n";
-$string = <STDIN>;
-for (my $i=0; $i != $number1; $i++) {
-print $string;
-}
-<STDIN>;
