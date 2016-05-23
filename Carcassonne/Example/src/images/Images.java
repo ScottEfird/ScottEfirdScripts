6575342af4b0566a209b0b6d1d52aef976e7dde5
scottefird <efirds@hotmail.com>
2016-05-23 16:47:51 -0600

    Done working on this for the night

---
+++ Carcassonne/Example/src/images/Images.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mainPackage.images;
+
+/**
+ *
+ * @author De Gregorio Daniele
+ */
+public class Images {
+
+}
--- Carcassonne/src/main.java
+++ Carcassonne/src/main.java
@@ -5,12 +5,14 @@ import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,9 +22,9 @@ public class main {
 	public static JPanel JPanel;
 
 	public static void main(String[] args) {
-		frame = new JFrame("Carcassonne");
+		frame = new JFrame("Carpet Solitaire");
 		frame.setVisible(true);
-		frame.setSize(1200, 600);
+		frame.setSize(1150, 495);
 		frame.setLayout(new BorderLayout());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
 		JPanel = new JPanel();
@@ -32,23 +34,28 @@ public class main {
 		
 		java.awt.EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				projectInit();
+				try {
+					setDeck();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		});
 	}
+
+	
 	/**
-	 * Generate names of files
+	 * Puts a card onto the frame.
+	 * @throws IOException 
 	 */
-	public static void projectInit() {
-		JLabel imgLabel = new JLabel(new ImageIcon("src/images/1.png"));
+	public static void setDeck() throws IOException {
+		BufferedImage myPicture = ImageIO.read(new File("src/images/1.png"));
+
+		JLabel wIcon = new JLabel(new ImageIcon(myPicture));
+		JPanel.add(wIcon);
 		
-		
-		Image image = Toolkit.getDefaultToolkit().getImage("src/images/1.png");
-		ImageIcon icon = new ImageIcon(image);				
-		JPanel.add(icon);
-		JPanel.revalidate();
-		JPanel.repaint();
-		
-		//Deck localDeck = new Deck();
+
 	}
 }
+
